359c518e428353b6be8b450feac5e8a98577893f
nic0mp <nico.mp215@gmail.com>
2021-12-09 09:59:02 -0600

    + .env

--- myApp/config.py
+++ myApp/config.py
@@ -13,3 +13,5 @@ class Config():
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')  
+    SQLALCHEMY_TRACK_NOTIFICAITONS = False
